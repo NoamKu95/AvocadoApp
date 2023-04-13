9b35de2e26c2dd49fb18bf178913b6220b0f5616
Noam Kurtzer <noamkurtzer@gmail.com>
2023-04-13 18:18:07 +0300

    App init

---
+++ AvocadoApp/AvocadoApp/AvocadoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoAppApp.swift
+//  AvocadoApp
+//
+//  Created by Noam Kurtzer on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
